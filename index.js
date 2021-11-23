575698c951c3436ce16553b0c1e7f227df89780b
taufikka <taufikul67@gmail.com>
2021-11-23 12:15:50 +0600

    added

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
 
         })
 
-        // DELETE product api
+        // DELETE product API
         app.delete('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
